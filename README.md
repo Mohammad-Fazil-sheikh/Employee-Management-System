be6c35835e6900f93eabff96513d8a3b9db940fe
Mohammad-Fazil-sheikh <mohammadehteshamsheikh5@gmail.com>
2024-12-23 19:28:09 +0530

    add

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+I Created this app with the help of React + Vite 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+  I used the REACT HOOK FORM for form handling
+  I used REACT TOASTIFY for toast alerts
+  I used REACT ICON for icons
 
-Currently, two official plugins are available:
+Data is Store in LocalStorage in JSON format
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+<!-- - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh -->
--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,3 @@
-// import React, { useState } from 'react';
-// import Login from './components/log/login';
-// import Admin from './components/log/admin';
-// import Emp from './components/log/emp';
-// import { setdata, getdata, data } from './utils/localstorage';
-// import { useContext } from 'react';
-// import { con } from './context/authercon';
-
-// function App() {
-//   setdata(data);
-//   const somthing = useContext(con)
-    
-
-//   const [currentComponent, setCurrentComponent] = useState('login'); 
-//   const [loginuser, setloginuser] = useState()
-//   const [user, setuser] = useState()
-  
-  
-//   const func = (email, pass) => {
-//     // Check if the user email and password match any in the data
-//     const  matchedUser = data.find(itm => itm.email === email && itm.pass === pass);
-    
-//     if (matchedUser) {
-      
-//       if (matchedUser.type === 'admin') {
-//         setCurrentComponent('admin');
-//         setloginuser(matchedUser);
-//         setuser(matchedUser);
-//         console.log(user);
-//         console.log({loginuser});
-//       } 
-      
-//       else if (matchedUser.type === 'employee') {
-//         setCurrentComponent('emp');
-//         setloginuser(matchedUser);
-//       }
-//     } else {
-      
-//       alert('Invalid email or password');
-//     }
-//   };
-
-//   return (
-//     <>
-//       {currentComponent === 'login' && <Login fun={func} />} 
-//       {currentComponent === 'admin' && <Admin />} 
-//       {currentComponent === 'emp' && <Emp data={loginuser}/>} 
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import React, { useState, useEffect, useContext } from 'react';
 import Login from './components/log/login';
 import Admin from './components/log/admin';
@@ -67,16 +11,13 @@ function App() {
   
 
   const [currentComponent, setCurrentComponent] = useState('login');
-  const [loginuser, setLoginUser] = useState(null); // Renamed to follow convention
+  const [loginuser, setLoginUser] = useState(null); 
 
   const func = (email, pass) => {
-    // Check if the user email and password match any in the data
     const matchedUser = data.find(itm => itm.email === email && itm.pass === pass);
 
     if (matchedUser) {
       setLoginUser(matchedUser);
-
-      // Change component based on user type
       if (matchedUser.type === 'admin') {
         setCurrentComponent('admin');
       } else if (matchedUser.type === 'employee') {
@@ -89,9 +30,8 @@ function App() {
 
   useEffect(() => {
     if (loginuser) {
-      // console.log('Login user:', loginuser); // Log the login user when it's set
     }
-  }, [loginuser]); // This will trigger whenever `loginuser` state changes
+  }, [loginuser]);
 
   return (
     <>
